5091163fa8dce92b7507e0b9b2cb133fb3c4ec1c
cloudtechdevsoul <139046669+cloudtechdevsoul@users.noreply.github.com>
2023-07-22 18:59:36 +0500

    SDSA

--- index.js
+++ index.js
@@ -1,36 +1,20 @@
-// ./src/index.js
-// importing the dependencies
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const helmet = require('helmet');
-const morgan = require('morgan');
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
 
-// defining the Express app
-const app = express();
-// defining an array to work as the database (temporary solution)
-const ads = [
-  {title: 'Hello, world (again)!'}
-];
+var LoremIpsum = require('lorem-ipsum').LoremIpsum;
 
-// adding Helmet to enhance your Rest API's security
-app.use(helmet());
-
-// using bodyParser to parse JSON bodies into JS objects
-app.use(bodyParser.json());
-
-// enabling CORS for all requests
-app.use(cors());
-
-// adding morgan to log HTTP requests
-app.use(morgan('combined'));
-
-// defining an endpoint to return all ads
-app.get('/', (req, res) => {
-  res.send(ads);
+var lorem = new LoremIpsum({
+  sentencesPerParagraph: {
+    max: 1,
+    min: 1
+  },
+  wordsPerSentence: {
+    max: 1,
+    min: 1
+  }
 });
 
-// starting the server
-app.listen(3001, () => {
-  console.log('listening on port 3001');
-});
+app.get('/', (req, res) => res.send("hello world"))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
